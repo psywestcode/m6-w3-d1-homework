534e6ad6c8f4376068b7cd764ba4e5de935a51ab
Sailesh Mohapatra <s.mohapatra5530@westcliff.edu>
2026-04-19 11:25:39 -0700

    add routers, action, reducers

---
+++ theme-app/src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import styled from "styled-components";
+import styled, { ThemeProvider } from "styled-components";
+import theme from "styled-theming";
+
+export const theme1 = theme("theme", {
+  light: "#fff",
+  dark: "#2d2d2d",
+});
+
+export const theme2 = theme("theme", {
+  light: "#000",
+  dark: "#fff",
+});
+
+const Container = styled.div`
+  background-color: ${theme1};
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+  align-items: center;
+  justify-content: center;
+  font-family: sans-serif;
+`;
+
+const SwitchTheme = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 50px;
+  height: 75px;
+`;
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <SwitchTheme>
+        <h1>Theme App</h1>
+        <p>
+          <input type="checkbox" /> Use Dark Theme
+        </p>
+      </SwitchTheme>
+      <Container>
+        <h2>Welcome!</h2>
+        <h3>Full Stack Web Development</h3>
+      </Container>
+    </React.Fragment>
+  );
+};
+
+export default App;
